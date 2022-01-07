498f2a35b3588312a7ad797ebdf436ca454a18b0
Arif Faysal <ayon.arif.10@gmail.com>
2022-01-06 20:07:42 -0500

    updated

--- src/components/class/Fragment/TableData.js
+++ src/components/class/Fragment/TableData.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 
 class TableData extends Component {
 
+    styleClass = (data) => {
+        if(data) {
+            return 'text-center border border-gray-300 text-green-600';
+        } else {
+            return 'text-center border border-gray-300 text-red-600';
+        }
+    }
+
     render() {
         return (
             <>
@@ -10,9 +18,9 @@ class TableData extends Component {
                     this.props.tableData.map((element, index) => (
                         <React.Fragment key={ index }>
                             <tr>
-                                <td className="text-center border border-gray-300">{ index }</td>
+                                <td className="text-center border border-gray-300">{ element.id }</td>
                                 <td className="text-center border border-gray-300">{ element.name }</td>
-                                <td className="text-center border border-gray-300">{ (element.name) ? 'Yes' : 'No' }</td>
+                                <td className={ this.styleClass(element.member) }>{ (element.member) ? 'Yes' : 'No' }</td>
                             </tr>
                         </React.Fragment>
                     ))
